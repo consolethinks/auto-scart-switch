9bc863b5519dbdfd0a68259fe3d9d144f108053c
Consolethinks <wewa@first-email.net>
2022-04-02 17:22:05 +0200

    initial commit for no ir branch

--- PIC/include/NEC_IR.h
+++
@@ -1,9 +0,0 @@
-#ifndef NEC
-#define NEC
-
-#include <stdint.h>
-
-short leadpulse_IR_verification();
-short read_IR_bit(uint8_t* );
-
-#endif
--- PIC/include/PULL_SW.h
+++
@@ -1,8 +0,0 @@
-#ifndef PULL_SW
-#define PULL_SW
-
-#include <stdint.h>
-
-void pulldown_switcher(volatile uint8_t *port_out, short select_out);
-
-#endif
